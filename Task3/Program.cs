6781b4362b837fdc22131e167f0d1d91aacc7484
Ринат <rinatmuhamedyarov1967@gmail.com>
2024-02-03 11:29:04 +0500

    add progr

---
+++ Task3/Program.cs
@@ -0,0 +1,12 @@
+﻿//int n = 10;
+int[] array = { 2, 5, 8, 4, 7, 9, 35, 67, 88, 3 };
+int i = 0;
+
+while (i < array.Length)
+{
+    if (array[i] % 2 == 0)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    i = i + 1;
+}
