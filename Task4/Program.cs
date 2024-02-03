2de9d7d4af4104a45770ef30275242174e3ad362
Ринат <rinatmuhamedyarov1967@gmail.com>
2024-02-03 11:40:46 +0500

    add PRGtask4

---
+++ Task4/Program.cs
@@ -0,0 +1,14 @@
+﻿//int n = 5;
+int[] array = { 2, 15, 8, 4, 7 };
+int i = 0;
+int max = array[0];
+
+while (i < array.Length)
+{
+    if (array[i] > max)
+    {
+        max = array[i];
+    }
+    i = i + 1;
+}
+        Console.Write(max);
